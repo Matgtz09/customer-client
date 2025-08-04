7a7f356b90f4ed1a10abb05fbb3ac2a428f19d22
Matthew gtz <matgtz96@gmail.com>
2025-08-03 20:48:54 -0700

    update env and config

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,15 @@
-let API_BASE_URL = "";
+// let API_BASE_URL = "";
 
-if (window.location.hostname === "localhost") {
-  API_BASE_URL = "http://localhost:3000";
-} else if (window.location.hostname.includes("staging")) {
-  API_BASE_URL = "https://staging-backend.fly.dev";
-} else {
-  API_BASE_URL = "https://api-backend.fly.dev";
-}
+// if (window.location.hostname === "localhost") {
+//   API_BASE_URL = "http://localhost:3000";
+// } else if (
+//   window.location.hostname.includes("firebaseapp.com") ||
+//   window.location.hostname.includes("web.app")
+// ) {
+//   API_BASE_URL = "https://rails-backend-420903537528.us-central1.run.app"; // ← paste here
+// } else {
+//   API_BASE_URL = "https://rails-backend-420903537528.us-central1.run.app";
+// }
 
-export { API_BASE_URL };
+// export { API_BASE_URL };
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
